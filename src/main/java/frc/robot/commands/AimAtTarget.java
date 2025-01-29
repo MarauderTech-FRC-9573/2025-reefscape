9cc28c2fcc9f04b09a1964b32de485af7f006f4a
9573maraudertech <162756411+9573maraudertech@users.noreply.github.com>
2025-01-29 12:44:15 -0800

    fresh version of vision.java. startcompetition error
    Co-Authored-By: Julian Garcia <69781917+julianalg@users.noreply.github.com>

--- src/main/java/frc/robot/commands/AimAtTarget.java
+++ src/main/java/frc/robot/commands/AimAtTarget.java
@@ -2,22 +2,20 @@ package frc.robot.commands;
 
 import java.util.Optional;
 
-import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.Vision.Cameras;
-import swervelib.SwerveDrive;
 
 public class AimAtTarget extends Command {
     CommandXboxController xboxController;
     Cameras photonCamera;
     SwerveSubsystem swerveDrive;
+    Optional<PhotonPipelineResult> resultO;
     
     public AimAtTarget(CommandXboxController xbox, Cameras camera, SwerveSubsystem swerve) {
         xboxController = xbox;
@@ -32,23 +30,22 @@ public class AimAtTarget extends Command {
     
     @Override
     public void execute() {
-        try { 
-            System.out.println("Pipeline getting results");
-            Optional<PhotonPipelineResult> resultO = photonCamera.getBestResult();
-            if (resultO.isPresent())
-            {
+        resultO = photonCamera.getBestResult();
+        if (resultO.isPresent()) {
+            try { 
                 var result = resultO.get();
                 if (result.hasTargets())
                 {
                     SmartDashboard.putNumber("ID Recognized", result.getBestTarget().getFiducialId());
                     double turn = -1.0 * result.getBestTarget().getYaw() * 0.021 * 0.3;
-                    SmartDashboard.putNumber("Turn Amt", turn);
+                    System.out.println("ID" + result.getBestTarget().getFiducialId());
+                    System.out.println("Yaw" + result.getBestTarget().getYaw());
+                    System.out.println("Turn Amt" + turn);
                     swerveDrive.getSwerveDrive().drive(new Translation2d(0, 0), Math.toDegrees(turn), isScheduled(), isFinished());
                 }  
-            } 
         } catch (Exception e) {
-            SmartDashboard.putNumber("ID Recognized", -9999);
+            System.out.println("No target found..." + e);
         }
-
+    }
     }
 }
