b3270a260e794f1fd81cf9d3ee82c7f51ecf8b74
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-01-31 12:29:23 -0800

    debug

--- src/main/java/frc/robot/commands/AimAtTarget.java
+++ src/main/java/frc/robot/commands/AimAtTarget.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import java.util.Optional;
 
 import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,6 +17,8 @@ public class AimAtTarget extends Command {
     Cameras photonCamera;
     SwerveSubsystem swerveDrive;
     Optional<PhotonPipelineResult> resultO;
+    PhotonTrackedTarget target;
+    PhotonPipelineResult reading;
     
     public AimAtTarget(CommandXboxController xbox, Cameras camera, SwerveSubsystem swerve) {
         xboxController = xbox;
@@ -25,27 +28,13 @@ public class AimAtTarget extends Command {
     
     @Override
     public void initialize() {
-
+        resultO = photonCamera.getBestResult();
+        reading = resultO.get();
+        System.out.println(reading.getBestTarget().getFiducialId());
     }
     
     @Override
     public void execute() {
-        resultO = photonCamera.getBestResult();
-        if (resultO.isPresent()) {
-            try { 
-                var result = resultO.get();
-                if (result.hasTargets())
-                {
-                    SmartDashboard.putNumber("ID Recognized", result.getBestTarget().getFiducialId());
-                    double turn = -1.0 * result.getBestTarget().getYaw() * 0.021 * 0.3;
-                    System.out.println("ID" + result.getBestTarget().getFiducialId());
-                    System.out.println("Yaw" + result.getBestTarget().getYaw());
-                    System.out.println("Turn Amt" + turn);
-                    swerveDrive.getSwerveDrive().drive(new Translation2d(0, 0), Math.toDegrees(turn), isScheduled(), isFinished());
-                }  
-        } catch (Exception e) {
-            System.out.println("No target found..." + e);
-        }
-    }
+
     }
 }
