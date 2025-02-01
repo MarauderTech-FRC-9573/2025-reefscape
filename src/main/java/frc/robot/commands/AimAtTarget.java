e5f7bf6b25142d5f565b40e344ef02a2c845e051
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-01-31 18:30:03 -0800

    aim and get in range of target
    Co-Authored-By: 9573maraudertech <162756411+9573maraudertech@users.noreply.github.com>

--- src/main/java/frc/robot/commands/AimAtTarget.java
+++ src/main/java/frc/robot/commands/AimAtTarget.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -24,6 +26,8 @@ public class AimAtTarget extends Command {
     double yaw;
     double turn;
     double initIMU;
+    double targetRange;
+    double drive;
     
     public AimAtTarget(CommandXboxController xbox, PhotonCamera camera, SwerveSubsystem swerve) {
         xboxController = xbox;
@@ -46,9 +50,12 @@ public class AimAtTarget extends Command {
                 targetId = target.getFiducialId();
                 yaw = target.getYaw();
                 turn = yaw * 0.021;
-    
-                swerveDrive.getSwerveDrive().drive(new Translation2d(0, 0), turn, isScheduled(), isFinished());
-    
+                
+                targetRange = PhotonUtils.calculateDistanceToTargetMeters(Units.inchesToMeters(11.5), Units.inchesToMeters(8.5), Units.degreesToRadians(-30), Units.degreesToRadians(target.getPitch()));
+                drive = targetRange * 1;
+
+                swerveDrive.getSwerveDrive().drive(new Translation2d(0, drive), turn, isScheduled(), isFinished());
+                
                 SmartDashboard.putNumber("ID", targetId);
                 SmartDashboard.putNumber("Yaw", yaw);
                 SmartDashboard.putNumber("Turn", turn);
