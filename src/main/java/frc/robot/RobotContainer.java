2c5b2c88b387cbd4b0c133955f321a6493d94b92
9573maraudertech <162756411+9573maraudertech@users.noreply.github.com>
2025-03-05 06:04:18 -0800

    elevator auto debug

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -130,7 +130,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+  public Command getAutomousCommand() {
     // An example command will be run in autonomous
     return autoChooser.getSelected();
   }
