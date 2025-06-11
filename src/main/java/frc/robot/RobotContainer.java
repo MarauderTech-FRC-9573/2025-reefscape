eb381e73084a614c7b1de2b5d2ed3fd278df2651
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-06-11 16:12:57 -0700

    vibe coded
    Co-Authored-By: Andy Ly <97271338+rake6682@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,22 +8,32 @@ import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.commands.ElevatorCommands.ElevatorManualControl;
 import frc.robot.commands.ElevatorCommands.ElevatorSetpointCommand;
+
 public class RobotContainer {
-    private final ElevatorSubsystem elevator = new ElevatorSubsystem();
-    private final CommandXboxController m_operatorController = new CommandXboxController(0);
-    public RobotContainer() {
-        configureBindings();
-    }
+  private final ElevatorSubsystem elevator = new ElevatorSubsystem();
+  private final CommandXboxController m_operatorController = new CommandXboxController(0);
 
-    private void configureBindings() {
-        // Example: Manual control using joystick input
-        m_operatorController.leftY().whileTrue(
-            new ElevatorManualControl(elevator, m_operatorController::getLeftY)
-        );
+  public RobotContainer() {
+    configureBindings();
+  }
 
-        // Example: Move to a specific setpoint
-        m_operatorController.a().onTrue(
-            new ElevatorSetpointCommand(elevator, ElevatorConstants.UPPER_ALGAE_ENCODER)
-        );
-    }
+  private void configureBindings() {
+    // Example: Manual control using joystick input
+    m_operatorController.leftStick().whileTrue(
+        new ElevatorManualControl(elevator, m_operatorController::getLeftY));
+
+    // Example: Move to a specific setpoint
+    m_operatorController.a().onTrue(
+        new ElevatorSetpointCommand(elevator, ElevatorConstants.UPPER_ALGAE_ENCODER));
+  }
+
+  // /**
+  // * Use this to pass the autonomous command to the main {@link Robot} class.
+  // *
+  // * @return the command to run in autonomous
+  // */
+  public Command getAutomousCommand() {
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
 }
