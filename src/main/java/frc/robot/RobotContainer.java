aa51ddc9bbacfebd028aa772ea9e2ef4f11cb234
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-06-11 17:10:29 -0700

    logs the controller
    Co-Authored-By: Andy Ly <97271338+rake6682@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,15 @@ import frc.robot.Constants.ElevatorConstants;
 import frc.robot.commands.ElevatorCommands.ElevatorManualControl;
 import frc.robot.commands.ElevatorCommands.ElevatorSetpointCommand;
 import frc.robot.commands.PivotCommands.PivotManualControl;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 public class RobotContainer {
   private final ElevatorSubsystem elevator = new ElevatorSubsystem();
   private final PivotSubsystem pivot = new PivotSubsystem();
   private final CommandXboxController m_operatorController = new CommandXboxController(0);
+
 
   public RobotContainer() {
     configureBindings();
@@ -19,9 +23,9 @@ public class RobotContainer {
 
   private void configureBindings() {
     // Example: Manual control using joystick input
-    m_operatorController.y().whileTrue(
-        new ElevatorManualControl(elevator, m_operatorController::getLeftY));
-
+    m_operatorController.y().whileTrue(new ParallelCommandGroup(
+        new ElevatorManualControl(elevator, m_operatorController::getLeftY),
+        new RunCommand(() -> SmartDashboard.putNumber("Left y: ", m_operatorController.getLeftY()))));
     // Example: Move to a specific setpoint
     m_operatorController.a().onTrue(
         new ElevatorSetpointCommand(elevator, ElevatorConstants.UPPER_ALGAE_ENCODER));
